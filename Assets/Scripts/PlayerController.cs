905e6e34e4a84cce4c85fb79532054ac2e9d7362
Jake Allport <jake.w.allport@gmail.com>
2023-11-03 14:08:38 -0400

    Crash Scene

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    private float speed = 5.0f;
+    private float speed = 25.0f;
     private float turnSpeed = 25.0f;
 
     private float forwardInput;
